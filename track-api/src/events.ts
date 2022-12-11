3b8b318e06af51e705d5e515a641fd5840eb6066
Yirbett Joseph <yirbett@gmail.com>
2022-12-11 11:19:13 -0400

    added buid scripts and ping event

--- track-api/src/events.ts
+++ track-api/src/events.ts
@@ -1,16 +1,17 @@
-const EVENTS={
-    CONNECTION:"connection",
-    UPDATE:"update",
-    UPDATE_POSITION_FAILED:"opdate_position_failed",
-    TRIP_START:"start_trip",
-    TRIP_START_SUCCESS:"trip_started",
-    TRIP_START_FAILED:"trip_start_failed",
-    TRIP_END:"trip_end",
-    TRIP_END_SUCCESS:"trip_end_success",
-    TRIP_END_FAIL:"trip_end_failed",
-    UPDATE_POSITION:"update_position",
-    DISCONNECT:"disconnect"
-}
-
+const EVENTS = {
+  CONNECTION: "connection",
+  UPDATE: "update",
+  PING: "ping",
+  PONG: "pong",
+  UPDATE_POSITION_FAILED: "opdate_position_failed",
+  TRIP_START: "start_trip",
+  TRIP_START_SUCCESS: "trip_started",
+  TRIP_START_FAILED: "trip_start_failed",
+  TRIP_END: "trip_end",
+  TRIP_END_SUCCESS: "trip_end_success",
+  TRIP_END_FAIL: "trip_end_failed",
+  UPDATE_POSITION: "update_position",
+  DISCONNECT: "disconnect",
+};
 
 export default EVENTS;
--- track-api/src/main.ts
+++ track-api/src/main.ts
@@ -11,6 +11,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const socketIO = require("socket.io");
+const TrackingCache: Tracker = new Tracker();
 const io = socketIO(server, {
   cors: {
     origin: "*",
@@ -18,14 +19,14 @@ const io = socketIO(server, {
   },
 });
 
-const TrackingCache: Tracker = new Tracker();
 app.use(cors("*"));
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ success: true });
 });
 
 io.on(EVENTS.CONNECTION, (socket: Socket) => {
-  socket.on("test", (name) => console.log("hey klk " + name));
+  socket.on(EVENTS.PING, (data: string) => io.to(socket.id).emit(EVENTS.PONG, data));
 
   socket.on(EVENTS.TRIP_START, (tripDto: TripDTO) => {
     //add  tripp dto in memory
